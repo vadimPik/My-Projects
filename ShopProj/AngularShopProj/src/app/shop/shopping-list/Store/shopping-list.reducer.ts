2acf49b2fb4992f3f961888504212e4f13753240
Vadim Pik <VadimPik@users.noreply.github.com>
2020-06-03 19:13:08 +0300

    Delete unnecessary code

--- ShopProj/AngularShopProj/src/app/shop/shopping-list/Store/shopping-list.reducer.ts
+++ ShopProj/AngularShopProj/src/app/shop/shopping-list/Store/shopping-list.reducer.ts
@@ -44,55 +44,31 @@ export function ShoppingListReducer(state: ShoppingListState = initialState, act
 
     //Filter- create new list without the product in index that want to delete.
     //action.payload - Index in product that want to delete.
-    //product.index - index of the current product. filter function pass on all products.
     case ShoppingListActions.DELETE_PRODUCT_SUCCESS:
       const productToDelete = state.products[action.payload];
 
       return {
         ...state,
-        //products: delete cloneDeep(state.products)[action.payload],
+
           products: state.products.filter((product) => {
             return product !== productToDelete;
         }),
         totalprice: state.totalprice - (productToDelete.ProductPrice * productToDelete.Quantity),
         numberOfProducts: state.numberOfProducts - productToDelete.Quantity,
-      //  ShoppingListID: action.payload.ShoppingListID,
         Status: 0,
-      //  Quantity: productToDelete.Quantity - 1
       };
 
     case ShoppingListActions.DELETE_PRODUCT_FAILED:
         return { ...state, ...action };
     
     case ShoppingListActions.UPDATE_QUANTITY_SUCCESS:
-     
-      const productToUpdate = state.products[action.payload.IndexToUpdate];
-
-      
-
-
-
-
-     // productToUpdate.Quantity = action.payload.Quantity;
-      let numericQuantity : number;
-
-      numericQuantity = parseInt( productToUpdate.Quantity.toString());
-      let totalPriceWithoutChangedItem = state.totalprice - (productToUpdate.ProductPrice * numericQuantity);
-      let totalNumberOfProductsWithoutChangedItem = state.numberOfProducts - numericQuantity;
- 
+    
       return {
         ...state,
-        //products: delete cloneDeep(state.products)[action.payload],
-     //  ...state.products[action.payload.IndexToUpdate].Quantity =  action.payload.Quantity;
-        //   products: state.products.filter((product) => {
-        //     return product !== productToUpdate;
-        // }),
-    //  products: [...state.products, {[action.payload.IndexToUpdate]: productToUpdate}],
+
         products: action.payload.UpdatedProducts,
         totalprice: action.payload.UpdatedTotalPrice,
-        numberOfProducts: parseInt(action.payload.UpdatedNumberOfProducts.toString())
-     //   totalprice: totalPriceWithoutChangedItem + (productToUpdate.ProductPrice * action.payload.Quantity),
-     //   numberOfProducts: state.numberOfProducts + action.payload.Quantity
+        numberOfProducts: action.payload.UpdatedNumberOfProducts
       };
 
       case ShoppingListActions.UPDATE_QUANTITY_FAILED:
