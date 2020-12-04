be667faa2bae90db56aec3ec212c4564d21c2cc9
vadim <vadimpik@gmail.com>
2020-12-04 13:45:38 +0200

    h

---
+++ Wpf_mvvm/Wpf_mvvm/Model/ModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Wpf_mvvm.Model
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Wpf_mvvm/Wpf_mvvm/Model/SchoolStudent.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace Wpf_mvvm.Model
+{
+    public class SchoolStudent : ModelBase
+    {
+        private string _studentTz;
+
+        public string StudentTz
+        {
+            get { return _studentTz; }
+            set
+            {
+                _studentTz = value;
+                NotifyPropertyChanged("StudentTz");
+            }
+        }
+
+        private string _studentFirstName;
+
+        public string StudentFirstName
+        {
+            get { return _studentFirstName; }
+            set
+            {
+                _studentFirstName = value;
+                NotifyPropertyChanged("StudentFirstName");
+            }
+
+        }
+
+        private string _studentLastName;
+
+        public string StudentLastName
+        {
+            get { return _studentLastName; }
+            set
+            {
+                _studentLastName = value;
+                NotifyPropertyChanged("StudentLastName");
+            }
+
+        }
+
+        private string _studentClassNumber;
+        public string StudentClassNumber
+        {
+            get { return _studentClassNumber; }
+            set
+            {
+                _studentClassNumber = value;
+                NotifyPropertyChanged("StudentClassNumber");
+            }
+
+        }
+
+
+    }
+}
