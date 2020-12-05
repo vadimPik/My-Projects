b2d38da67eb2654c3321a79b5e17af06dcd4f4c8
vadim <vadimpik@gmail.com>
2020-12-05 20:20:26 +0200

    c

---
+++ Wpf_mvvm/Wpf_mvvm/ViewModel/MyMessageBoxService .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_mvvm.ViewModel
+{
+    public class MyMessageBoxService : IMessageBoxService
+    {
+        public void ShowMessage(string text, string header)
+        {
+            MessageBox.Show(text, header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Wpf_mvvm/Wpf_mvvm/ViewModel/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_mvvm.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, args);
+            }
+        }
+    }
+}
